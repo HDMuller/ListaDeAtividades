ec751573c80eed7bb42a0776311109df6cdd97ee
luisbigu <53881633+luisbigu@users.noreply.github.com>
2019-08-07 19:42:34 -0300

    Add files via upload

---
+++ agenda.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 03-Ago-2019 às 04:06
+-- Versão do servidor: 10.1.37-MariaDB
+-- versão do PHP: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `agenda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tarefa`
+--
+
+CREATE TABLE `tarefa` (
+  `nome` varchar(20) DEFAULT NULL,
+  `descricao` varchar(100) DEFAULT NULL,
+  `data_agenda` date DEFAULT NULL,
+  `codigo` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tarefa`
+--
+ALTER TABLE `tarefa`
+  ADD PRIMARY KEY (`codigo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
